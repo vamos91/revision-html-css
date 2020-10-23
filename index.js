f7a5924070596c579dbe7507587ef90b3331f118
simplon-marseille <acardnicolas@hotmail.com>
2020-10-23 14:54:07 +0200

    revision HTML-CSS

---
+++ index.js
@@ -0,0 +1,3 @@
+// ScrollReveal().reveal('h1', {delay: 400, origin: 'top', distance: '40px'});
+// ScrollReveal().reveal('h2', { delay: 800 });
+// ScrollReveal().reveal('.bouton', { delay: 1200 });
